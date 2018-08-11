a9ea13001facc345e6fe81975ed56a5d7b0b28ba
whaenel <vonhenninghausen@web.de>
2018-08-11 22:58:15 +0200

    Added Tests for HttpConnect and removed unused tests

--- UsingHTTP/src/main/java/de/nikolauspflege/bbw/fiae/http/HttpConnect.java
+++ UsingHTTP/src/main/java/de/nikolauspflege/bbw/fiae/http/HttpConnect.java
@@ -10,56 +10,35 @@ import jdk.incubator.http.HttpResponse;
 
 public class HttpConnect {
 
+	public static void run(String[] args) {
+		ManagedHTTPConnection conn = new ManagedHTTPConnection();
+		HttpResponse<String> response =null;
+		URI myUri;
+		try {
+			myUri = new URI("http://httpbin.org/get");
+			if (args.length == 1) {
+				myUri = new URI(args[0]);
+			}
+
+			response = conn.sendHttpsGet(myUri);
+		} catch (IOException | InterruptedException | URISyntaxException e) {
+			System.out.println("request failed  " );
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("headers = " + response.headers().toString());
+		System.out.println("length =" + response.body().length());
+		if (response.body().length() > 0 ) {
+			System.out.println(response.body().toString());
+		}
+	}
 	
 	public static void main(String[] args) {
-		/*
-		HttpResponse<String> strResponseS = null;
-		try {
-			strResponseS = sendHttps(new URI("https://javadeveloperzone.com/java-9/java-9-module-example/"));
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-
-		*/
-		
-		// TODO Auto-generated method stub
-        try {
-    		HttpClient client = HttpClient.newBuilder().build();
-			HttpRequest request = HttpRequest.newBuilder(new URI("http://httpbin.org/get"))
-			        .GET()
-			        .version(HttpClient.Version.HTTP_1_1)
-			        .build();
-	        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandler.asString());
-	        System.out.println("Status code: " + response.statusCode());
-	        System.out.println("Response Body: " + response.body());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]) ;
 		}
-        
-        
-        System.out.println("HTTPS: ------------------------------------------------ " );
-		HttpResponse<String> strResponse = null;
-		ManagedHTTPConnection con = new ManagedHTTPConnection();
-		try {
-			strResponse = con.sendHttpsGet(new URI("https://javadeveloperzone.com/java-basic/java-9-features/java-9-module-example/"));
-			//strResponse = sendHttps(new URI("https://javadeveloperzone.com/java-9/java-9-module-example/"));
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	
+		System.out.println("-----------------------") ;
+		run(args);
 
         
 	}
---
+++ UsingHTTP/src/test/java/de/nikolauspflege/bbw/fiae/HttpConnectTest.java
@@ -0,0 +1,38 @@
+package de.nikolauspflege.bbw.fiae;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.nikolauspflege.bbw.fiae.http.HttpConnect;
+
+class HttpConnectTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testRun() {
+		
+		String[] args = { "http://httpbin.org/get?name=Hugo" };
+		HttpConnect.run(args );
+	}
+
+}
